0cbdb06f1823c2ca0171bb671e6e58b537e11eef
Oussama SAIDI <saidi.oussama2004@gmail.com>
2026-03-10 01:49:28 +0000

    Updates..

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+import os
+
+API_KEY = "secret-api-key-12345"
+
 def add(a, b):
     return a + b
+
+def read_config():
+    password = "admin123"
+    return password
+
+def debug_mode():
+    return True
